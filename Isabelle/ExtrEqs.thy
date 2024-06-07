019ea5b570957e7ed3a4e90a6c6c3eb73b972dda
solrun <solalola@gmail.com>
2024-06-07 16:15:31 +0200

    trying out some methods for finding info

--- Isabelle/ExtrEqs.thy
+++ Isabelle/ExtrEqs.thy
@@ -1,4 +1,5 @@
-theory ExtrEqs imports Main Templates
+theory ExtrEqs 
+  imports Main "HOL-Library.Tree" "HOL-Library.Stream" (* Templates *)
 begin
 declare [[ML_print_depth=10000]]
 
@@ -10,7 +11,11 @@ val badeqpattern = Find_Theorems.Pattern(Proof_Context.read_term_pattern
                    @{context} "Pure.eq");
 val undefpattern = Find_Theorems.Pattern(Proof_Context.read_term_pattern 
                    @{context} "Trueprop(_ = undefined)");
-(*Try to avoid some utility theorems, associated with various tools *)
+val thmpattern = Find_Theorems.Pattern(Proof_Context.read_term_pattern
+                 @{context} "Trueprop(_)");
+(*Try to avoid some utility theorems, associated with various tools - have another look at whether more things should be added to the list *)
+(*Filter out theorems with more than one "." to avoid imports
+and stick with those defined in the current theory file*)
 val badnames =  ["*nitpick*","*Nitpick*", "*full_exhaustive*", "*_def*", "Enum.finite*",
                  "*Quickcheck*", "ATP*","*Nunchaku*","*Code*", "*BNF*","*SMT*","*.*.*"]
 val unwanted_names : (bool * Term.term Find_Theorems.criterion) list = 
@@ -34,9 +39,32 @@ fun get_all_eqs namefilter ctxt =
                         else NONE) eqs
     end;
 
+fun get_all_thms namefilter ctxt =
+  let val (_,thms) = Find_Theorems.find_theorems ctxt NONE (SOME 16000) true
+                    ([(true,thmpattern),(false,badeqpattern),(false,undefpattern)]@[(true,Find_Theorems.Name (namefilter))]@unwanted_names)
+    in map_filter (fn (factref,thm) => 
+                        if  Thm.prop_of thm |> has_vars (* Remove plain definitions *)
+                        then SOME (Facts.ref_name factref, thm)
+                        else NONE) thms
+    end;
 
+fun get_thms_withname namefilter ctxt =
+  let val (_,thms) = Find_Theorems.find_theorems ctxt NONE (SOME 16000) true
+                    [(true,Find_Theorems.Name (namefilter))]
+    in thms
+    end;
+fun get_noneq_thms namefilter ctxt =
+  let val (_,thms) = Find_Theorems.find_theorems ctxt NONE (SOME 16000) true
+                    ([(true,thmpattern),(false,eqpattern),(false,badeqpattern),(false,undefpattern)]@[(true,Find_Theorems.Name (namefilter))]@unwanted_names)
+    in map_filter (fn (factref,thm) => 
+                        if  Thm.prop_of thm |> has_vars (* Remove plain definitions *)
+                        then SOME (Facts.ref_name factref, thm)
+                        else NONE) thms
+    end;
+fun get_tydefs t = Term.add_vars (Thm.full_prop_of t) [];
 (* Remove things that are non-equalities and meta-level equalities *)
-fun drop_unwanted thm = 
+(*
+fun drop_unwanted thm =
   let 
     fun is_not (Const ("HOL.Not", _)) = true
   | is_not _ = false;
@@ -52,14 +80,42 @@ fun drop_unwanted thm =
   in
     ((not o is_not o Term.head_of) t) andalso not_metaeq (Term.head_of t)
   end;
-
+*)
 (* do we want to compile more data about these theorems ?*)
-fun template_eqs namefilter ctxt = 
+(*
+fun template_eqs namefilter ctxt =
     List.map (fn (s,thm) => (s,thm,thm2template thm))
        (#1 ((List.partition (fn (_,thm) => Thm.no_prems thm))
        (rev (get_all_eqs namefilter ctxt))
      ));
 
+fun template_thms namefilter ctxt = 
+    List.map (fn (s,thm) => (s,thm,thm2template thm))
+       (rev (get_all_thms namefilter ctxt));
+*)
+(* Trying some methods out *)
+
+val t = @{thm Tree.mirror_mirror};
+
+(*full_prop_of lists names and types of all components
+prop_of is kind of the same? *)
+val fp = Thm.full_prop_of t;
+
+(*
+Term.add_vars
+gives vars in a structure (indexname * typ) list
+
+Term.add_consts lists out constants in a (string * typ) list
+the string part is the full name like "Orderings.ord_class.less_eq"
+*)
+Term.add_vars fp [];
+Term.add_consts fp [];
+get_tydefs t;
+(* All thms that contain the given string in their name *)
+get_thms_withname "mirror" @{context};
+(*val ths = template_thms "Tree" @{context}*)
 \<close>
 
+thm Tree.acomplete_def
+thm Tree.mirror.simps
 end
